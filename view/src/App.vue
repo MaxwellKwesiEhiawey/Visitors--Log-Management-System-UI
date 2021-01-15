5fc04e43fef37f9975e8faa300de5405a6a6c621
MaxwellKwesiEhiawey <72410727+MaxwellKwesiEhiawey@users.noreply.github.com>
2021-01-15 11:51:59 +0000

    updated

--- view/src/App.vue
+++ view/src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+ <img alt="" src="./assets/logo.png">
+  <HelloWorld msg="Welcome ."/>
+</div>
 </template>
 
 <script>
